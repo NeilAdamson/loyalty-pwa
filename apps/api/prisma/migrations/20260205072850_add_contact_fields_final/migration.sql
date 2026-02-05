baf43908b6d8f7e20b1de79a04ef2589051ec559
NeilAdamson <neil@effectivevideo.co.za>
2026-02-05 13:10:27 +0200

    Fix contact_fields migration: UPDATE NULLs before SET NOT NULL

--- apps/api/prisma/migrations/20260205072850_add_contact_fields_final/migration.sql
+++ apps/api/prisma/migrations/20260205072850_add_contact_fields_final/migration.sql
@@ -5,9 +5,13 @@
   - Made the column `contact_phone` on table `vendors` required. This step will fail if there are existing NULL values in that column.
 
 */
+-- Update existing NULLs before making columns NOT NULL
+UPDATE "vendors" SET "contact_name" = '' WHERE "contact_name" IS NULL;
+UPDATE "vendors" SET "contact_phone" = '' WHERE "contact_phone" IS NULL;
+
 -- AlterTable
-ALTER TABLE "vendors" ADD COLUMN     "contact_surname" TEXT NOT NULL DEFAULT '',
-ALTER COLUMN "contact_name" SET NOT NULL,
-ALTER COLUMN "contact_name" SET DEFAULT '',
-ALTER COLUMN "contact_phone" SET NOT NULL,
-ALTER COLUMN "contact_phone" SET DEFAULT '';
+ALTER TABLE "vendors" ADD COLUMN "contact_surname" TEXT NOT NULL DEFAULT '';
+ALTER TABLE "vendors" ALTER COLUMN "contact_name" SET DEFAULT '';
+ALTER TABLE "vendors" ALTER COLUMN "contact_name" SET NOT NULL;
+ALTER TABLE "vendors" ALTER COLUMN "contact_phone" SET DEFAULT '';
+ALTER TABLE "vendors" ALTER COLUMN "contact_phone" SET NOT NULL;
